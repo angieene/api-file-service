51fa5673e723e1c04676cf58631c14bef9ce0698
angieene <angela.nerukh@gmail.com>
2023-04-02 22:16:40 +0300

    bugfix cors error

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000',
+    credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
